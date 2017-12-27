2b6ccb4d46668986742282a19f0eb7b5bec9cb46
sidney aranha <sidneyaranhasilva@gmail.com>
2017-12-27 11:57:10 -0200

    commite alterar e remover

--- estudo/src/main/java/br/com/sidney/dao/ContatoDao.java
+++ estudo/src/main/java/br/com/sidney/dao/ContatoDao.java
@@ -22,8 +22,7 @@ public class ContatoDao {
 	}
 
 	public void adicionar(Contato contato) {
-		String sql = "insert into contatos "
-				+ "(nome,email,endereco,dataNascimento)" + " values (?,?,?,?)";
+		String sql = "insert into contatos " + "(nome,email,endereco,dataNascimento)" + " values (?,?,?,?)";
 
 		try {
 			// prepared statement para inserção
@@ -34,8 +33,7 @@ public class ContatoDao {
 			stmt.setString(1, contato.getNome());
 			stmt.setString(2, contato.getEmail());
 			stmt.setString(3, contato.getEndereco());
-			stmt.setDate(4, new Date(contato.getDataNascimento()
-					.getTimeInMillis()));
+			stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
 
 			// executa
 			stmt.execute();
@@ -47,107 +45,136 @@ public class ContatoDao {
 
 	}
 
-	public List<Contato> getLista() { 
-		 List<Contato> contatos = new ArrayList<Contato>();
-		 try {
+	public List<Contato> getLista() {
+		List<Contato> contatos = new ArrayList<Contato>();
+		try {
 			PreparedStatement stmt = this.connection.prepareStatement("select * from contatos");
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()){
-				 // criando o objeto Contato 10 
-				Contato contato = new Contato(); 
+			while (rs.next()) {
+				// criando o objeto Contato 10
+				Contato contato = new Contato();
 				contato.setId(rs.getLong("id"));
-				contato.setNome(rs.getString("nome")); 
-				contato.setEmail(rs.getString("email")); 
-				contato.setEndereco(rs.getString("endereco")); 
-				
-				
-				 // montando a data através do Calendar 
-				Calendar data = Calendar.getInstance(); 
-				data.setTime(rs.getDate("dataNascimento")); 
-				contato.setDataNascimento(data); 
-				
-				 // adicionando o objeto à lista 
-				contatos.add(contato); 
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+
+				// montando a data através do Calendar
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+
+				// adicionando o objeto à lista
+				contatos.add(contato);
 			}
-			
-			 rs.close(); 
-			 stmt.close(); 
-			 return contatos; 
+
+			rs.close();
+			stmt.close();
+			return contatos;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			throw new DAOException();
 		}
-		 
-		 
-	 }
-	
-	public List<Contato> getListaByNome(String nome) { 
-		 List<Contato> contatos = new ArrayList<Contato>();
-		 try {
+
+	}
+
+	public List<Contato> getListaByNome(String nome) {
+		List<Contato> contatos = new ArrayList<Contato>();
+		try {
 			PreparedStatement stmt = this.connection.prepareStatement("select * from contatos where nome like ?");
-			stmt.setString(1, "%" + nome + "%"); 
+			stmt.setString(1, "%" + nome + "%");
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()){
-				 // criando o objeto Contato 10 
-				Contato contato = new Contato(); 
+			while (rs.next()) {
+				// criando o objeto Contato 10
+				Contato contato = new Contato();
 				contato.setId(rs.getLong("id"));
-				contato.setNome(rs.getString("nome")); 
-				contato.setEmail(rs.getString("email")); 
-				contato.setEndereco(rs.getString("endereco")); 
-				
-				
-				 // montando a data através do Calendar 
-				Calendar data = Calendar.getInstance(); 
-				data.setTime(rs.getDate("dataNascimento")); 
-				contato.setDataNascimento(data); 
-				
-				 // adicionando o objeto à lista 
-				contatos.add(contato); 
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+
+				// montando a data através do Calendar
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+
+				// adicionando o objeto à lista
+				contatos.add(contato);
 			}
-			
-			 rs.close(); 
-			 stmt.close(); 
-			 return contatos; 
+
+			rs.close();
+			stmt.close();
+			return contatos;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			throw new DAOException();
 		}
-		 
-		 
-	 }
-	
-	public List<Contato> getListaById(Long id) { 
-		 List<Contato> contatos = new ArrayList<Contato>();
-		 try {
+
+	}
+
+	public List<Contato> getListaById(Long id) {
+		List<Contato> contatos = new ArrayList<Contato>();
+		try {
 			PreparedStatement stmt = this.connection.prepareStatement("select * from contatos where id = ?");
-			stmt.setLong(1, id); 
+			stmt.setLong(1, id);
 			ResultSet rs = stmt.executeQuery();
-			while(rs.next()){
-				 // criando o objeto Contato 10 
-				Contato contato = new Contato(); 
+			while (rs.next()) {
+				// criando o objeto Contato 10
+				Contato contato = new Contato();
 				contato.setId(rs.getLong("id"));
-				contato.setNome(rs.getString("nome")); 
-				contato.setEmail(rs.getString("email")); 
-				contato.setEndereco(rs.getString("endereco")); 
-				
-				
-				 // montando a data através do Calendar 
-				Calendar data = Calendar.getInstance(); 
-				data.setTime(rs.getDate("dataNascimento")); 
-				contato.setDataNascimento(data); 
-				
-				 // adicionando o objeto à lista 
-				contatos.add(contato); 
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+
+				// montando a data através do Calendar
+				Calendar data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data);
+
+				// adicionando o objeto à lista
+				contatos.add(contato);
 			}
-			
-			 rs.close(); 
-			 stmt.close(); 
-			 return contatos; 
+
+			rs.close();
+			stmt.close();
+			return contatos;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			throw new DAOException();
 		}
-		 
-		 
-	 }
+
+	}
+
+	public void alterar(Contato contato) {
+		String sql = "update contatos set nome=?, email=?, endereco=?," + "dataNascimento=? where id=?";
+
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+			stmt.setLong(5, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e); 
+		}
+
+	}
+	
+	public void remover(Contato contato){
+		String sql = "delete from contatos where id = ?";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e); 
+		}
+	}
+
 }
